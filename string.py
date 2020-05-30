abb30dc3c47faf3baed865ee777a23cbe3504863
Nawaf <nawafsqaud@gmail.com>
2020-05-31 00:41:30 +0300

    String

---
+++ string.py
@@ -0,0 +1,25 @@
+name = "Nawaf" # string
+# In Python , for defind varibale you can define with type the name and equal = , only !
+first_name = "Nawaf"
+last_name = "Python"
+print(first_name + " " + last_name)
+
+
+age = 18 # integer , number
+value = 1.4 # float
+decision = True # boolen
+print(name)
+print("The type of name is : ", type(name))
+print("and id is : ", id(name)) # iD var's is PID
+print("with Hex : ", hex(id(name)))
+
+lower = "nawaf"
+print("Captil.. UPPER() =  ", lower.upper()) # Capital
+
+capt = "NAWAF"
+print("LOwer .. ", capt.lower()) # Lower
+
+print(lower.capitalize()) # first letter Capital
+
+lang = "PHP Is The Best !"
+print(lang.replace("PHP", "Python")) # old and new
