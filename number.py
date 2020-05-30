2e56c2bf56416dd916c0f261e7b4c8fccbac7302
Nawaf <nawafsqaud@gmail.com>
2020-05-31 00:56:52 +0300

    add Number

---
+++ number.py
@@ -0,0 +1,24 @@
+print(10 + 20) # make Math
+print("10" + "20") # will be string
+# print(10 + '20') # will be Error {Kind: TypeError : Unspported addition string with number )
+print(10 + int('20')) # casting, convert type, from string to integer
+
+# Math Opreator
+
+print(20 + 20) # add
+print(21 - 20) # minus
+print(2 * 5) # multying
+print(10 / 2) # Dividing
+
+# power of 10
+print(3** 10) # 3 infected 3 person with covid-19, will be infected 3 others and other 3 infected .. even 10 times
+
+num1 = 10.1 # float
+num2 = 20 # integer
+num3 = num1 + num2
+print(num3, type(num3))
+
+num2 += 1 # will add 1 to 20
+# or with another way ..
+num2 = num2 + 1
+print(num2)
